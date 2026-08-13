4449bdf1d7d80d5b0e00a25b7081a9b714c62be1
mchipperfield <189554036+mchipperfield@users.noreply.github.com>
2026-08-13 14:55:18 +0100

    fix issue with firestore not completing transactions

--- firestore/privacy.go
+++ firestore/privacy.go
@@ -17,32 +17,41 @@ func NewPrivacyService(client *firestore.Client) *PrivacyService {
 
 func (s *PrivacyService) DeleteUserData(ctx context.Context, userID string) error {
 	return s.client.RunTransaction(ctx, func(ctx context.Context, tx *firestore.Transaction) error {
-		query := s.client.Collection("players").Where("user_id", "==", userID)
-
-		iter := tx.Documents(query)
+		playerQuery := s.client.Collection("players").Where("user_id", "==", userID)
+		playerIter := tx.Documents(playerQuery)
+		var playerRefs []*firestore.DocumentRef
 		for {
-			doc, err := iter.Next()
+			doc, err := playerIter.Next()
+			if err == iterator.Done {
+				break
+			}
 			if err != nil {
-				if err == iterator.Done {
-					break
-				}
 				return err
 			}
-			if err := tx.Delete(doc.Ref); err != nil {
+			playerRefs = append(playerRefs, doc.Ref)
+		}
+
+		allianceQuery := s.client.Collection("alliances").Where("code_channel.user_id", "==", userID)
+		allianceIter := tx.Documents(allianceQuery)
+		var allianceRefs []*firestore.DocumentRef
+		for {
+			doc, err := allianceIter.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				return err
+			}
+			allianceRefs = append(allianceRefs, doc.Ref)
+		}
+
+		for _, ref := range playerRefs {
+			if err := tx.Delete(ref); err != nil {
 				return err
 			}
 		}
-		query = s.client.Collection("alliances").Where("code_channel.user_id", "==", userID)
-		iter = tx.Documents(query)
-		for {
-			doc, err := iter.Next()
-			if err != nil {
-				if err == iterator.Done {
-					break
-				}
-				return err
-			}
-			if err := tx.Update(doc.Ref, []firestore.Update{
+		for _, ref := range allianceRefs {
+			if err := tx.Update(ref, []firestore.Update{
 				{Path: "code_channel.user_id", Value: "DELETED_USER"},
 			}); err != nil {
 				return err
